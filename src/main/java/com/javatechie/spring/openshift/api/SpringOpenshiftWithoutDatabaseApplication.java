7b9d9a96f94f6d4114846bd1d451d5c8eb90004a
JessicaBarai <81428391+JessicaBarai@users.noreply.github.com>
2021-06-24 10:49:45 +0200

    Add files via upload

---
+++ src/main/java/com/javatechie/spring/openshift/api/SpringOpenshiftWithoutDatabaseApplication.java
@@ -0,0 +1,29 @@
+package com.javatechie.spring.openshift.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringOpenshiftWithoutDatabaseApplication {
+	
+	@GetMapping("/")
+	public String welcome() {
+		return "welcome to java!";
+		
+	}
+	
+	@GetMapping("test/{input}")
+	public String congrats(@PathVariable String input) {
+		return "hi" + input + "success!";
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenshiftWithoutDatabaseApplication.class, args);
+	}
+
+}
